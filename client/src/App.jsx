7b77268d7fbc8fc40a1f7a4b9263b631ea4365e3
PHRCOOK <pabloelleproso@gmail.com>
2024-01-20 13:38:13 -0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@ import { useState } from "react";
 import "./components/css-modules/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return;
 }
 
 export default App;
