cceea0ab8cb20167bf3ffa079af56302ed0d0dbc
PHRCOOK <pabloelleproso@gmail.com>
2024-01-23 12:36:06 -0300

    ESTRUCTURA FRONT

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import "./components/css-modules/index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-const container = document.getElementById("root");
-const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
