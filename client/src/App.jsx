8dde3f78fc93eaefaba2778dcca73f97f60e5855
PHRCOOK <pabloelleproso@gmail.com>
2024-01-24 13:10:44 -0300

    ACTUALIZACION DE RUTAS FRONT

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Nav from "./components/nav/nav";
-import Detail from "./components/detail/detail";
-import Footer from "./components/footer/footer";
-import Home from "./components/home/home";
-import Form from "./components/form/form";
+import Nav from "./components/nav/Nav";
+import Detail from "./components/detail/Detail";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import Form from "./components/form/Form";
+import Product from "./components/product/Product";
+import Shoppingcart from "./components/shoppingcart/Shoppingcart";
 import pathroutes from "./components/helpers/pathroutes";
 import "./components/css-modules/App.css";
 import axios from "axios";
@@ -18,6 +20,8 @@ function App() {
         <Route path={pathroutes.HOME} element={<Home />} />
         <Route path={pathroutes.DETAIL} element={<Detail />} />
         <Route path={pathroutes.FORM} element={<Form />} />
+        <Route path={pathroutes.PRODUCT} element={<Product />} />
+        <route path={pathroutes.SHOPPINGCART} element={<Shoppingcart />} />
       </Routes>
       <Footer />
     </div>
