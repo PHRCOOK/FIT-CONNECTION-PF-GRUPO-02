01fa083d2c3ce564db9e2489f7e6ac29066830ae
Slasho01 <josuerendon01@gmail.com>
2024-01-25 19:36:48 -0300

    Arreglando desorden

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,18 +1,18 @@
 const { Router } = require('express');
 const usersRouter = require('./usersRouter');
-const categoriesRoute = require('./categoryRoute.js')
 const instructorRouter = require('./instructorsRouter');
 const feedBackRouter = require('./feedBackRouter');
+const categoriesRoute = require('./categoryRoute.js')
 const router = Router();
 
 // Definir rutas aquí:
 
 router.use("/users", usersRouter)
 
-router.use('/categorias', categoriesRoute); 
-
 router.use("/instructors", instructorRouter);
 
 router.use("/feedbacks", feedBackRouter);
+
+router.use('/categorias', categoriesRoute); 
 
 module.exports = router;
