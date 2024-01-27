56fdf29329f25db69b00ab91e1226e6fb5901364
canabaldev1 <canabal.dev1@gmail.com>
2024-01-26 22:08:04 -0500

    Update store.jsx

---
+++ client/src/views/store.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Filters from "../components/filters/filters";
+import Cards from "../components/cards/cards";
+
+function Store() {
+  return (
+    <div>
+      {" "}
+      store
+      <Filters />
+      <Cards />
+    </div>
+  );
+}
+
+export default Store;
