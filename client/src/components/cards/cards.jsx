2b761d005b3a16a96ce7607373d4d76930240ded
PHRCOOK <pabloelleproso@gmail.com>
2024-01-30 01:49:36 -0300

    NAVBAR Y DETAIL

--- client/src/components/cards/cards.jsx
+++ client/src/components/cards/cards.jsx
@@ -1,7 +1,8 @@
-// import React from "react";
+import React from "react";
 import AppCard from "../card/card";
 import { useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Cards() {
   const productsToShow = useSelector((state) => state.productsToShow);
@@ -11,17 +12,19 @@ function Cards() {
       {productsToShow.map((item) => {
         return (
           <Col xs="12" md="6" lg="4" className="p-3" key={item.id}>
-            <AppCard
-              id={item.id}
-              name={item.name}
-              price={item.price}
-              description={item.description}
-              status={item.status}
-              code={item.code}
-              image_url={item.image_url}
-              stock={item.stock}
-              category={item.category}
-            />
+            <Link to={`/detail/${item.id}`}>
+              <AppCard
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                description={item.description}
+                status={item.status}
+                code={item.code}
+                image_url={item.image_url}
+                stock={item.stock}
+                category={item.category}
+              />
+            </Link>
           </Col>
         );
       })}
