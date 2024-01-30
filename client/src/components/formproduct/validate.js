2d13e055ea78d35bab76cbcb7ab7228b809e57e6
Ileana <ferchu91garcia@gmail.com>
2024-01-30 00:36:06 -0300

    commit de Arreglo de Formulario con mas validaciones.

--- client/src/components/formproduct/validate.js
+++ client/src/components/formproduct/validate.js
@@ -3,6 +3,12 @@ export const validate = (input) => {
   if (!input.name) {
     errors.name = "It cannot be empty.";
   }
+  if (isNaN(input.price)) {
+    errors.price = "Price must be a valid number.";
+  }
+  if (input.price < 0) {
+    errors.price = "The price cannot be a negative number.";
+  }
   if (!input.price) {
     errors.price = "It cannot be empty.";
   }
@@ -18,11 +24,20 @@ export const validate = (input) => {
   if (!input.image_url) {
     errors.image_url = "Upload an image.";
   }
+  if (input.stock < 0) {
+    errors.stock = "The stock cannot be a negative number.";
+  }
+  if (isNaN(input.stock)) {
+    errors.stock = "Stock must be a valid number.";
+  }
   if (!input.stock) {
     errors.stock = "It cannot be empty.";
   }
   if (!input.category_id) {
     errors.category_id = "Select a category.";
+  }
+  if (!input.status) {
+    errors.status = "Select one status.";
   }
   return errors;
 };
