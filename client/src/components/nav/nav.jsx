487f9917cd44891a01c5ba645e95d20dbed790e6
PHRCOOK <pabloelleproso@gmail.com>
2024-01-30 01:36:11 -0300

    NAVBAR

--- client/src/components/nav/nav.jsx
+++ client/src/components/nav/nav.jsx
@@ -8,40 +8,30 @@ export default function AppBar() {
   const location = useLocation();
 
   const linksData = [
-    {
-      path: pathroutes.PRODUCT,
-      title: "Productos",
-      show: location.pathname !== pathroutes.PRODUCT,
-    },
-    { path: pathroutes.SERVICE, title: "Servicios", show: true },
-    {
-      path: pathroutes.FORMPRODUCT,
-      title: "Crear productos",
-      show: location.pathname === pathroutes.PRODUCT,
-    },
-    { path: "/shopping-card", title: "Carrito de compras", show: true },
-    { path: "/staff", title: "Conocer staff", show: true },
-    { path: "/login", title: "Login", show: true },
-    { path: "/sign-up", title: "Registrate", show: true },
+    { path: pathroutes.PRODUCT, title: "Productos" },
+    { path: pathroutes.SERVICE, title: "Servicios" },
+    { path: pathroutes.FORMPRODUCT, title: "Crear productos" },
+    { path: "/shopping-card", title: "Carrito de compras" },
+    { path: "/staff", title: "Conocer staff" },
+    { path: "/login", title: "Login" },
+    { path: "/sign-up", title: "Registrate" },
   ];
 
   const links = [];
   for (const linkData of linksData) {
-    if (linkData.show) {
-      const active = location.pathname === linkData.path;
-      links.push(
-        <LinkContainer key={linkData.path} to={linkData.path}>
-          <Nav.Link
-            active={active}
-            className={`rounded fw-bold px-2 mx-1 my-md-1 ${
-              active ? "bg-primary" : ""
-            }`}
-          >
-            {linkData.title}
-          </Nav.Link>
-        </LinkContainer>
-      );
-    }
+    const active = location.pathname === linkData.path;
+    links.push(
+      <LinkContainer key={linkData.path} to={linkData.path}>
+        <Nav.Link
+          active={active}
+          className={`rounded fw-bold px-2 mx-1 my-md-1 ${
+            active ? "bg-primary" : ""
+          }`}
+        >
+          {linkData.title}
+        </Nav.Link>
+      </LinkContainer>
+    );
   }
 
   return (
