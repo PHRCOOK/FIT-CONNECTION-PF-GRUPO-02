59c8d83f63b9a0442ff6f4f21fbff5dfbdc01473
PHRCOOK <pabloelleproso@gmail.com>
2024-01-29 22:22:30 -0300

    FRONT

--- client/src/components/nav/nav.jsx
+++ client/src/components/nav/nav.jsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 import logo from "../../assets/img/logo-nav.png";
 import pathroutes from "../helpers/pathroutes";
 import { LinkContainer } from "react-router-bootstrap";
-import { Container, Nav, Navbar, Image } from 'react-bootstrap'
+import { Container, Nav, Navbar, Image } from "react-bootstrap";
 
 export default function AppBar() {
   const location = useLocation();
@@ -10,46 +10,46 @@ export default function AppBar() {
   const linksData = [
     { path: pathroutes.PRODUCT, title: "Productos" },
     { path: pathroutes.SERVICE, title: "Servicios" },
-    { path: pathroutes.FORMPRODUCT, title: "Crear productos"},
+    { path: pathroutes.FORMPRODUCT, title: "Crear productos" },
     { path: "/shopping-card", title: "Carrito de compras" },
     { path: "/staff", title: "Conocer staff" },
     { path: "/login", title: "Login" },
-    { path: "/sign-up", title: "Registrate" }
-  ]
+    { path: "/sign-up", title: "Registrate" },
+  ];
 
-  const links = []
+  const links = [];
   for (const linkData of linksData) {
-    const active = location.pathname === linkData.path
+    const active = location.pathname === linkData.path;
     links.push(
       <LinkContainer key={linkData.path} to={linkData.path}>
         <Nav.Link
           active={active}
-          className={`rounded fw-bold px-2 mx-1 my-md-1 ${active ? 'bg-primary' : ''}`}
-        >{linkData.title}</Nav.Link>
+          className={`rounded fw-bold px-2 mx-1 my-md-1 ${
+            active ? "bg-primary" : ""
+          }`}
+        >
+          {linkData.title}
+        </Nav.Link>
       </LinkContainer>
-    )
+    );
   }
 
   return (
-    <Navbar
-      collapseOnSelect
-      bg="secondary"
-      expand="lg">
+    <Navbar collapseOnSelect bg="secondary" expand="lg">
       <Container>
         <LinkContainer to={pathroutes.HOME}>
-          <Navbar.Brand >
+          <Navbar.Brand>
             <Image
               src={logo}
               alt="Home"
               className="border border-2 border-light"
-              roundedCircle />
+              roundedCircle
+            />
           </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="navbar-options" />
-        <Navbar.Collapse id="navbar-options" >
-          <Nav className="ms-auto">
-            {links}
-          </Nav>
+        <Navbar.Collapse id="navbar-options">
+          <Nav className="ms-auto">{links}</Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
