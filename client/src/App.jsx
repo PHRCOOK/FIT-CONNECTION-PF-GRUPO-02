3a8b292b2379b5c0784edcf77fe4636d7cc6d108
PHRCOOK <pabloelleproso@gmail.com>
2024-01-31 14:11:38 -0300

    AUTENTIFICAION DE TERCEROS CON AUTH0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,15 @@ import AppBar from "./components/nav/nav";
 import Detail from "./components/detail/detail";
 import Footer from "./components/footer/footer";
 import Home from "./views/home/home";
+import Login from "./views/login/login";
 import Form from "./components/userform/userform";
-import Product from "./components/product/product";
 import Shoppingcart from "./views/shoppingcart/shoppingcart";
 import Services from "./views/services/services";
 import FormProduct from "./components/formproduct/formproduct";
 import UserForm from "./components/userform/userform";
 import pathroutes from "./components/helpers/pathroutes";
 import Store from "./views/store";
-import "./App.scss"
+import "./App.scss";
 import { Container } from "react-bootstrap";
 import axios from "axios";
 
@@ -24,6 +24,7 @@ function App() {
       <Container className="py-3 min-vh-100">
         <Routes>
           <Route path={pathroutes.HOME} element={<Home />} />
+          <Route path={pathroutes.LOGIN} element={<Login />} />
           <Route path={pathroutes.DETAIL} element={<Detail />} />
           <Route path={pathroutes.FORM} element={<Form />} />
           <Route path={pathroutes.PRODUCT} element={<Store />} />
