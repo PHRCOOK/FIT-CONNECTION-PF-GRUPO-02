6a3be536f2b7cb632f7941cf3b1bf5b6df4d41a8
Deras879 <derycard59@gmail.com>
2024-01-31 21:41:42 -0500

    Implementada ruta shopping cart

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,38 +1,14 @@
-<<<<<<< HEAD
 const express = require("express");
 const usersRouter = require("./usersRouter");
 const instructorRouter = require("./instructorsRouter");
 const feedBackRouter = require("./feedBackRouter");
 const productsRouter = require("./productsRouter");
 const categoryRouter = require("./categoryRoute");
-const shoppingCartRouter = require("./shoppingCartRouter");
-=======
-const express = require('express');
-const usersRouter = require('./usersRouter');
-const instructorRouter = require('./instructorsRouter');
-const feedBackRouter = require('./feedBackRouter');
-const productsRouter = require('./productsRouter');
-const categoryRouter = require('./categoryRoute');
->>>>>>> 91c8ccb09a678645c4a12d97b61033a934e76361
+const shoppingCartRoute = require("./shoppingCartRouter");
 const router = express.Router();
 
 // Definir rutas aquí:
 
-<<<<<<< HEAD
-router.use("/users", usersRouter);
-
-router.use("/instructors", instructorRouter);
-
-router.use("/feedbacks", feedBackRouter);
-
-router.use("/products", productsRouter);
-
-router.use("/categories", categoryRouter);
-
-router.use("/shoppingCart", shoppingCartRouter);
-
-module.exports = router;
-=======
 router.use("/api/users", usersRouter);
 
 router.use("/api/instructors", instructorRouter);
@@ -43,7 +19,6 @@ router.use("/api/products", productsRouter);
 
 router.use("/api/categories", categoryRouter);
 
-
+router.use("/api/shoppingCart", shoppingCartRoute);
 
 module.exports = router;
->>>>>>> 91c8ccb09a678645c4a12d97b61033a934e76361
