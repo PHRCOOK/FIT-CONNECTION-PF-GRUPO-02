ebf0530afbe129ccd4ba746a523c8160cc1400b7
PHRCOOK <pabloelleproso@gmail.com>
2024-02-02 11:36:53 -0300

    recercion de cambios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AppBar from "./components/nav/nav";
 import Detail from "./components/detail/detail";
 import Footer from "./components/footer/footer";
 import Home from "./views/home/home";
+import Login from "./views/login/login";
 import Form from "./components/userform/userform";
-import Product from "./components/product/product";
 import Shoppingcart from "./views/shoppingcart/shoppingcart";
 import Services from "./views/services/services";
 import FormProduct from "./components/formproduct/formproduct";
 import UserForm from "./components/userform/userform";
 import pathroutes from "./components/helpers/pathroutes";
 import Store from "./views/store";
-import Admin from "./administrator/admin/admin";
 import "./App.scss";
 import { Container } from "react-bootstrap";
 import axios from "axios";
@@ -19,13 +18,13 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
-  const location = useLocation();
   return (
     <>
-      {!location.pathname.startsWith("/admin") && <AppBar />}
+      <AppBar />
       <Container className="py-3 min-vh-100">
         <Routes>
           <Route path={pathroutes.HOME} element={<Home />} />
+          <Route path={pathroutes.LOGIN} element={<Login />} />
           <Route path={pathroutes.DETAIL} element={<Detail />} />
           <Route path={pathroutes.FORM} element={<Form />} />
           <Route path={pathroutes.PRODUCT} element={<Store />} />
@@ -34,7 +33,6 @@ function App() {
           <Route path={pathroutes.FORMPRODUCT} element={<FormProduct />} />
           <Route path={pathroutes.REGISTER} element={<UserForm />} />
           <Route path={pathroutes.STORE} element={<Store />} />
-          <Route path="/admin/*" element={<Admin />} />
         </Routes>
       </Container>
       <Footer />
