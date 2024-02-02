78104253c029ea3ccb7a789f909b7af341a9acdd
PHRCOOK <pabloelleproso@gmail.com>
2024-02-02 13:02:31 -0300

    M

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import Nav from "./components/nav/nav";
+import { Routes, Route, useLocation } from "react-router-dom";
+import AppBar from "./components/nav/nav";
 import Detail from "./components/detail/detail";
 import Footer from "./components/footer/footer";
 import Home from "./views/home/home";
+import Login from "./views/login/login";
 import Form from "./components/userform/userform";
-import Product from "./components/product/product";
-import Shoppingcart from "./components/shoppingcart/shoppingcart";
-import Services from "./components/services/services";
+import Shoppingcart from "./views/shoppingcart/shoppingcart";
+import Services from "./views/services/services";
 import FormProduct from "./components/formproduct/formproduct";
 import UserForm from "./components/userform/userform";
 import pathroutes from "./components/helpers/pathroutes";
 import Store from "./views/store";
-import "./components/css-modules/App.css";
+import Admin from "./administrator/admin/admin";
+import "./App.scss";
+import { Container } from "react-bootstrap";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
@@ -19,11 +21,12 @@ axios.defaults.baseURL = "http://localhost:3001";
 function App() {
   const location = useLocation();
   return (
-    <div>
-      <Nav />
-      <div className="container">
+    <>
+      {!location.pathname.startsWith("/admin") && <AppBar />}
+      <Container className="py-3 min-vh-100">
         <Routes>
           <Route path={pathroutes.HOME} element={<Home />} />
+          <Route path={pathroutes.LOGIN} element={<Login />} />
           <Route path={pathroutes.DETAIL} element={<Detail />} />
           <Route path={pathroutes.FORM} element={<Form />} />
           <Route path={pathroutes.PRODUCT} element={<Store />} />
