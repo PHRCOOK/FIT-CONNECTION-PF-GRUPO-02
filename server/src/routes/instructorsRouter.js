fb0d02f23ee7ba3b75f0441748acc95c01eaca30
Ileana <ferchu91garcia@gmail.com>
2024-02-03 00:20:11 -0300

    mosifique los controllers para enviar la respuesta correcta, no solo texto de la accion

--- server/src/routes/instructorsRouter.js
+++ server/src/routes/instructorsRouter.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
-const {createInstructorHandler, updateInstructorHandler} = require("../handlers/instructorHandlers");
-
+const {
+  createInstructorHandler,
+  updateInstructorHandler,
+  getInstructorHandler,
+  deleteInstructorsHandler,
+} = require("../handlers/instructorHandlers");
 
 const instructorRouter = Router();
 
-
+instructorRouter.get("/", getInstructorHandler);
 instructorRouter.post("/", createInstructorHandler);
-
 instructorRouter.put("/:id", updateInstructorHandler);
-
+instructorRouter.delete("/delete/:id", deleteInstructorsHandler);
 
 module.exports = instructorRouter;
