7cf8bc059835427378c4b17b8aa4705df61b3966
canabaldev1 <canabal.dev1@gmail.com>
2024-02-05 22:36:26 -0500

    Agregada variable PORT al index

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const server = require("./src/server.js");
 const { conn } = require("./src/db.js");
+const { PORT } = process.env;
 
 conn
   .sync({ force: false })
   .then(() => {
-    server.listen(3001, () => {
-      console.log("listening at 3001");
+    server.listen(PORT, () => {
+      console.log(`listening at ${PORT}`);
     });
   })
   .catch((error) => console.log(error)); // Se agrega para que muestre cualquier error de forma mas explicita..
