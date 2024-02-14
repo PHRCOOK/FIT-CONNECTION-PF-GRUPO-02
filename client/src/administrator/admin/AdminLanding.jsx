5947593d809618a31b7720cdc20827b81216444e
canabaldev1 <canabal.dev1@gmail.com>
2024-02-14 18:01:04 -0500

    agregada landing

--- client/src/administrator/admin/AdminLanding.jsx
+++ client/src/administrator/admin/AdminLanding.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import AdminNavbar from "../components/adminnavbar/adminnavbar";
 
 function AdminLanding() {
   const navigate = useNavigate();
 
   return (
     <div>
+      <AdminNavbar />
       <button onClick={() => navigate(`/admin/categories/`)}>Categories</button>
       <button onClick={() => navigate(`/admin/instructor/`)}>Instructor</button>
     </div>
