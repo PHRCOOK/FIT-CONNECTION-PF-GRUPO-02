14ad6ab97feef0b3c743bfd4b0f728c4dfa107cb
Slasho01 <josuerendon01@gmail.com>
2024-02-17 11:42:56 -0300

    autorizacion

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,19 +1,13 @@
-const { User } = require("../src/db");
 const authorization = async (req, res, next) => {
-  const { user_id } = req.body;
   try {
-    if(user_id) {
-      const { is_admin } = await User.findByPk(user_id);
-      if (is_admin === true) {
+    const isAdmin  = req.headers.is_admin === 'true';
+       if (isAdmin) {
         // Si el usuario es administrador, permite el acceso a la siguiente ruta
         next()
       } else {
         // Si el usuario no es administrador, envía un mensaje de error
         res.status(403).send('Acceso denegado. Debes ser administrador para acceder a esta ruta.');
       }
-    }else{
-      res.status(400).send("no hay informacion")
-    }
   } catch (error) {
     console.error('Error al buscar el usuario en la base de datos:', error);
     res.status(500).send('Error interno del servidor.');
