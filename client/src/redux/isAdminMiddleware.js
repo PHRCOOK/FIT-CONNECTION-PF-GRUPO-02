83a11138fc1c66ecd62e1ceb5379d6ad09a72562
Slasho01 <josuerendon01@gmail.com>
2024-02-17 16:09:05 -0300

    proteccion de rutas

---
+++ client/src/redux/isAdminMiddleware.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import {
+  GET_ALL_CATEGORIES,
+  POST_PRODUCT,
+  APPLY_FILTER,
+  RESET_FILTER,
+  EMPTY_FILTER,
+  DELETE_PRODUCT,
+  GET_ALL_PRODUCTS,
+  PUT_PRODUCT,
+  DELETE_CATEGORY,
+  POST_CATEGORY,
+  PUT_CATEGORY,
+  GET_ALL_INSTRUCTORS,
+  DELETE_INSTRUCTOR,
+  POST_INSTRCUTOR,
+  PUT_INSTRUCTOR,
+  POST_USER,
+  FETCH_USER_INFO,
+  GET_ALL_USER,
+  EMPTY_ALL_USER,
+  FETCH_CURRENT_USER,
+  SET_IS_ADMIN,
+} from "./actionsTypes";
+
+
+const isAdminMiddleware = store => next => action => {
+  const { isAdmin } = store.getState();
+  console.log("Hola",store.getState())
+  // Lista de acciones que requieren verificación de isAdmin
+  const actionsToCheck = [GET_ALL_PRODUCTS /* otras acciones aquí */];
+  if (actionsToCheck.includes(action.type) && !isAdmin) {
+    throw new Error('El usuario no es administrador');
+  }
+
+  if (isAdmin) {
+    axios.defaults.headers.common['is_admin'] = isAdmin;
+  } else {
+
+    delete axios.defaults.headers.common['is_admin'];
+  }
+  return next(action);
+};
+
+export default isAdminMiddleware;
