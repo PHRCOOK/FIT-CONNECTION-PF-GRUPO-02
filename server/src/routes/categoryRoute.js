55403a36d49d029755825b6d5c71e639077a77f0
Slasho01 <josuerendon01@gmail.com>
2024-02-17 00:57:21 -0300

    Pull

--- server/src/routes/categoryRoute.js
+++ server/src/routes/categoryRoute.js
@@ -5,19 +5,10 @@ const {
   putCategoriesHandler,
 } = require("../handlers/categoryHandler");
 const categoryRouter = Router();
-<<<<<<< HEAD
 const { authorization } = require('../../utils/auth')
 
 categoryRouter.get("/", authorization, getAllCategoriesHandler);
 categoryRouter.put("/:id", authorization, putCategoriesHandler);
-categoryRouter.delete("/:id", authorization, deleteCategoriesHandler);
 categoryRouter.post("/", authorization, postCategoriesHandler);
-=======
-// const { jwtCheck } = require("../../utils/auth");
-
-categoryRouter.get("/", getAllCategoriesHandler);
-categoryRouter.put("/:id", putCategoriesHandler);
-categoryRouter.post("/", postCategoriesHandler);
->>>>>>> a0d7b42f82db16dfd6f9e2f83e343eca7c019ee6
 
 module.exports = categoryRouter;
