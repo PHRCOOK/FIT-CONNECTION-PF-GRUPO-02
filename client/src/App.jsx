495ad203bfc4511b7ecd191a73004149c74bf505
PHRCOOK <pabloelleproso@gmail.com>
2024-02-20 02:54:19 -0300

    CIERRE DE SESION CUANDO ESTA INACTIVO

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -50,23 +50,6 @@ function App() {
       dispatch(fetchUser(userData));
 
       axios
-        .post("/api/users", userData)
-        .then((response) => console.log(response))
-        .catch((error) => console.error(error));
-    }
-  }, [isAuthenticated, user, dispatch]);
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      const userData = {
-        name: user.name,
-        sub: user.sub,
-        email: user.email,
-      };
-
-      dispatch(fetchUser(userData));
-
-      axios
         .get("/api/users", { params: { email: user.email } })
         .then((response) => {
           const userWithSameEmail = response.data.Items.find(
