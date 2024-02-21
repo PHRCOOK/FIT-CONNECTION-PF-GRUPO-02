010abca35a8f7d88e16c4e4e4263d16933532def
PHRCOOK <pabloelleproso@gmail.com>
2024-02-21 12:55:42 -0300

    BORRADO COMPONENTE SERVICES DUPLICADO

--- client/src/views/services/services.jsx
+++ client/src/views/services/services.jsx
@@ -3,13 +3,15 @@ import MembershipsCards from "../../components/cards_memberships/cards_membershi
 import { useEffect } from "react";
 
 function services() {
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
-  return <div>
-    <h1>SERVICIOS</h1>
-    <MembershipsCards />
-  </div>;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div>
+      <h1>SERVICIOS</h1>
+      <MembershipsCards />
+    </div>
+  );
 }
 
 export default services;
