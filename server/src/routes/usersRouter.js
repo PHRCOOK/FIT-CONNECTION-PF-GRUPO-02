8965e2958cd3fbcbf4b60c9f80cb2ed568ac914a
Josue Rendon <72944648+Slasho01@users.noreply.github.com>
2024-02-23 14:13:32 -0300

    Update usersRouter.js

--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -7,20 +7,20 @@ const {
   getDetailHandler,
   getUserByEmailHandler,
 } = require("../handlers/usersHandlers");
-
+const { authorization } = require("../../utils/auth");
 const usersRouter = Router();
 
 usersRouter.post("/", createUserHandler);
 
-usersRouter.get("/", getActiveUsersHandler);
+usersRouter.get("/", authorization, getActiveUsersHandler);
 
-usersRouter.get("/inactive", getInactiveUsersHandler);
+usersRouter.get("/inactive", authorization, getInactiveUsersHandler);
 
 usersRouter.get("/:id", getDetailHandler);
 
 usersRouter.get("/email/:email", getUserByEmailHandler);
 
-usersRouter.put("/:id", updateUserHandler);
+usersRouter.put("/:id", authorization, updateUserHandler);
 
 
 module.exports = usersRouter;
