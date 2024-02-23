8feabf640c056bb9029ee7a20a8732da16be7242
Josue Rendon <72944648+Slasho01@users.noreply.github.com>
2024-02-23 14:06:29 -0300

    Autorizacion en el put y post

--- server/src/routes/instructorsRouter.js
+++ server/src/routes/instructorsRouter.js
@@ -5,14 +5,14 @@ const {
   getInstructorHandler,
   getInstructorByIDHandler,
 } = require("../handlers/instructorHandlers");
-
+const { authorization } = require("../../utils/auth");
 const upload = require("../services/multer");
 
 const instructorRouter = Router();
 
 instructorRouter.get("/", getInstructorHandler);
 instructorRouter.get("/:id", getInstructorByIDHandler);
-instructorRouter.post("/", upload.single('photo'), createInstructorHandler);
-instructorRouter.put("/:id", updateInstructorHandler);
+instructorRouter.post("/", authorization, upload.single('photo'), createInstructorHandler);
+instructorRouter.put("/:id", authorization, updateInstructorHandler);
 
 module.exports = instructorRouter;
