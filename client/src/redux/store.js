1cffadcf2edf11c797a91224f9fdbfc0839f5f2e
canabaldev1 <canabal.dev1@gmail.com>
2024-02-23 18:02:34 -0500

    cambio menor para deplot

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,10 +4,13 @@ import { reducer } from "./reducer";
 
 import { thunk } from "redux-thunk";
 
-import isAdminMiddleware from './isAdminMiddleware';
+import isAdminMiddleware from "./isAdminMiddleware";
 
 const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhacer(applyMiddleware(thunk, isAdminMiddleware )));
+const store = createStore(
+  reducer,
+  composeEnhacer(applyMiddleware(thunk, isAdminMiddleware))
+);
 
 export default store;
