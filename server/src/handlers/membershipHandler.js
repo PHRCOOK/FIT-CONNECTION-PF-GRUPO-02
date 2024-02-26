3eda2a7790de90addcf17ba32248cb82c9d3918d
canabaldev1 <canabal.dev1@gmail.com>
2024-02-25 21:57:43 -0500

    arreglado modificacion membresias

--- server/src/handlers/membershipHandler.js
+++ server/src/handlers/membershipHandler.js
@@ -1,64 +1,100 @@
-const { validateMembership } = require('../../utils/validations/validateMembership');
-const {getAllMemberships ,getMembershipById, createMembership, updateMembership, deleteMembership} = require('../controllers/membershipController');
+const {
+  validateMembership,
+} = require("../../utils/validations/validateMembership");
+const {
+  getAllMemberships,
+  getMembershipById,
+  createMembership,
+  updateMembership,
+  deleteMembership,
+} = require("../controllers/membershipController");
 
 const getAllMembershipsHandler = async (req, res) => {
-    try {
-        const response = await getAllMemberships();
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    }
+  try {
+    const response = await getAllMemberships();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const getMembershipByIdHandler = async (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
   try {
     const response = await getMembershipById(id);
     res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
 
 const createMembershipHandler = async (req, res) => {
-  const {name, price, duration, description, status} = req.body;
+  const { name, price, duration, description, status } = req.body;
   const image_url = req.file;
-  console.log("esto es name",name)
+  console.log(
+    "name",
+    name,
+    "price",
+    price,
+    "duration",
+    duration,
+    "description",
+    description,
+    "status",
+    status
+  );
   try {
-    validateMembership({name, price, duration, description})
-    const response = await createMembership(name, price, duration, description, status, image_url);
+    validateMembership({ name, price, duration, description });
+    const response = await createMembership(
+      name,
+      price,
+      duration,
+      description,
+      status,
+      image_url
+    );
     res.status(201).json(response);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
 
 const updateMembershipHandler = async (req, res) => {
-    const {id} = req.params;
-    const {name, price, duration, description, status} = req.body;
-    const image_url = req.file;
-    try {
-        const response = await updateMembership(id, {name, price, duration, image_url, description, status});
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  const { id } = req.params;
+  const { name, price, duration, description, status } = req.body;
+  const image_url = req.file;
+  console.log(id);
+  console.log(req.body);
+  console.log(req.file);
+  try {
+    const response = await updateMembership(id, {
+      name,
+      price,
+      duration,
+      image_url,
+      description,
+      status,
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const deleteMembershipHandler = async (req, res) => {
-    const {id} = req.params;
-    try {
-        const response = await deleteMembership(id);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  const { id } = req.params;
+  try {
+    const response = await deleteMembership(id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
-    getAllMembershipsHandler,
-    getMembershipByIdHandler,
-    createMembershipHandler,
-    updateMembershipHandler,
-    deleteMembershipHandler
-}
+  getAllMembershipsHandler,
+  getMembershipByIdHandler,
+  createMembershipHandler,
+  updateMembershipHandler,
+  deleteMembershipHandler,
+};
--- server/src/routes/membershipsRouter.js
+++ server/src/routes/membershipsRouter.js
@@ -1,13 +1,32 @@
-const { Router } = require('express');
-const upload = require('../services/multer');
+const { Router } = require("express");
+const upload = require("../services/multer");
 
-const {getAllMembershipsHandler ,getMembershipByIdHandler, createMembershipHandler, updateMembershipHandler, deleteMembershipHandler} = require('../handlers/membershipHandler');
+const {
+  getAllMembershipsHandler,
+  getMembershipByIdHandler,
+  createMembershipHandler,
+  updateMembershipHandler,
+  deleteMembershipHandler,
+} = require("../handlers/membershipHandler");
+
 const membershipsRouter = Router();
+
 const { authorization } = require("../../utils/auth");
-membershipsRouter.get('/', getAllMembershipsHandler);
-membershipsRouter.get('/:id', getMembershipByIdHandler);
-membershipsRouter.post('/', authorization, upload.single('image_url'), createMembershipHandler);
-membershipsRouter.put('/update/:id', authorization, updateMembershipHandler);
-membershipsRouter.delete('/delete/:id', authorization, deleteMembershipHandler);
+
+membershipsRouter.get("/", getAllMembershipsHandler);
+membershipsRouter.get("/:id", getMembershipByIdHandler);
+membershipsRouter.post(
+  "/",
+  authorization,
+  upload.single("image_url"),
+  createMembershipHandler
+);
+membershipsRouter.put(
+  "/update/:id",
+  authorization,
+  upload.single("image_url"),
+  updateMembershipHandler
+);
+membershipsRouter.delete("/delete/:id", authorization, deleteMembershipHandler);
 
 module.exports = membershipsRouter;
